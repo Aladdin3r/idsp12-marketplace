db61972c5007f6e8c209033447825257b9823f07
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-15 10:31:00 -0700

    add about us section

---
+++ Components/HeadArea/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function HeadArea(){
+    return (<>
+        <Head>
+          <title>Stuff n Stuff</title>
+          <meta name="description" content="The modern and safe marketplace to earn your trust and connect with real people." />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/stuffnstuff_footer.webp" />
+        </Head>
+    </>)
+}
