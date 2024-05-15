8f4cf6a6bec1de25b82038141d50e6be8528af6f
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-15 09:55:35 -0700

    upload the video

---
+++ .history/Components/Video/index_20240515094921.js
@@ -0,0 +1,17 @@
+
+
+export default function Video () {
+
+    <video width="640" height="480" controls preload="none">
+      <source  src="/video/MarketingVideoIDSP.mp4" type="video/mp4" />
+      <track
+        src="/video/MarketingVideoIDSP.mp4"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+      />
+      Your browser does not support the video tag.
+    </video>
+
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Image from "next/image";
+import Video from "@/Components/Video";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
         <div className="container-fluid">
           <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
             <div className="container">
-              <a className="navbar-brand" href="#"><Image src="/stuffnstuff_footer.webp" width={100} height={100}/></a>
+              <a className="navbar-brand" href="#"><Image src="/stuffnstuff_footer.webp" width={100} height={100} /></a>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
@@ -44,25 +45,28 @@ export default function Home() {
         </div>
 
         <div className="jumbotron mb-5 ms-5">
-          <Image src="/stuffnstuff_footer.webp" width={500} height={400}/>
+          <Image src="/stuffnstuff_footer.webp" width={500} height={400} />
           <p className="lead">The modern and safe marketplace to earn your trust and connect with real people.</p>
-          <hr className="my-4"/>
+          <hr className="my-4" />
           <p className="lead">
             <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
           </p>
         </div>
 
+        <div className="jumbotron mb-6 ms-6">
+          <Video />
+        </div>
 
         <div id="contact" class="container-sm">
           <h4 className="mb-3">Contact Us</h4>
           <form>
             <div className="mb-3">
               <label htmlFor="name" className="form-label">Name</label>
-              <input type="text" className="form-control custom-width" id="name" placeholder="Enter your name"/>
+              <input type="text" className="form-control custom-width" id="name" placeholder="Enter your name" />
             </div>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Email address</label>
-              <input type="email" className="form-control custom-width" id="email" placeholder="name@example.com"/>
+              <input type="email" className="form-control custom-width" id="email" placeholder="name@example.com" />
             </div>
             <div className="mb-3">
               <label htmlFor="message" className="form-label">Message</label>
