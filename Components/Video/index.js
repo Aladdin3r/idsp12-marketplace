c4478aa465258c1db49e5c8e443defa5b5b7aaa8
timothy0618 <87256320+timothy0618@users.noreply.github.com>
2024-05-15 10:02:25 -0700

    update video screen size

--- Components/Video/index.js
+++ Components/Video/index.js
@@ -2,7 +2,7 @@
 
 export default function Video() {
   return (<>
-    <video width="960" height="720" controls preload="none" autoPlay loop>
+    <video width="960" height="540" controls preload="none" autoPlay loop>
       <source src="/video/MarketingVideoIDSP.mp4" type="video/mp4"  />
 
       Your browser does not support the video tag.
