8a31c968d578d036123217edc9c9d2dbb2845627
Rawan Aladdin <raladdin@my.bcit.ca>
2024-05-16 12:37:46 -0700

    tab name

--- Components/HeadArea/index.js
+++ Components/HeadArea/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function HeadArea(){
     return (<>
         <Head>
-          <title>Stuff n Stuff</title>
+          <title>SafeExchange</title>
           <meta name="description" content="The modern and safe marketplace to earn your trust and connect with real people." />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/RegularLogo.svg" />
